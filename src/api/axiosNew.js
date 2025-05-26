1c7158367e43f187aed28994e76cbbe6335f1d33
Er-Siddharth <sidshinde391999@gmail.com>
2025-05-26 12:03:50 +0530

    old server

--- src/api/axiosNew.js
+++ src/api/axiosNew.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://your-json-server.onrender.com"
-    : "http://localhost:3001";
-
 const axiosNew = axios.create({
-  baseURL,
+  baseURL: "http://localhost:3001/", // Correct base URL
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
