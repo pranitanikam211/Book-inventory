8c6eff0f06e4e3944621a04ecd17128d1c6ecec7
Er-Siddharth <sidshinde391999@gmail.com>
2025-05-26 12:22:15 +0530

    Update axios baseURL for production

--- src/api/axiosNew.js
+++ src/api/axiosNew.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://book-inventory-3.onrender.com"
+    : "http://localhost:3001"; // for local dev
+
 const axiosNew = axios.create({
-  baseURL: "http://localhost:3001/", // Correct base URL
+  baseURL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
